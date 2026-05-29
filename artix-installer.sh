5fb38dfec5eeaece7fb6ff6c3079669b7c5d29c3
mc-724634 <mcanida09@gmail.com>
2026-05-29 19:03:02 -0400

    Update artix-installer.sh

--- artix-installer.sh
+++ artix-installer.sh
@@ -183,7 +183,7 @@ pacman -Sy \
   pipewire-dinit pipewire-pulse-dinit wireplumber-dinit \
   flatpak \
   discord telegram-desktop steam gamemode lib32-gamemode \
-  avahi avahi-dinit \
+  avahi avahi-dinit fastfetch \
   ufw ufw-dinit \
   plasma plasma-meta
 
